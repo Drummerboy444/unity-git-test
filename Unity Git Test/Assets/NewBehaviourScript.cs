7465f244f6deee624c99d7866f2557a211f0a920
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-09-11 22:05:49 +0100

    Script changes colour of object on start

--- Unity Git Test/Assets/NewBehaviourScript.cs
+++ Unity Git Test/Assets/NewBehaviourScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Renderer renderer;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        renderer = gameObject.GetComponent<Renderer>();
+        renderer.material.color = Color.red;
     }
 }
