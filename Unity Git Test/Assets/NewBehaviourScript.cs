6da702966fa3250ef6421aed6ef38e1fd6d9f051
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-09-11 22:18:29 +0100

    Changes script to randomly change colour every second

--- Unity Git Test/Assets/NewBehaviourScript.cs
+++ Unity Git Test/Assets/NewBehaviourScript.cs
@@ -6,9 +6,26 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private Renderer renderer;
 
+    private float timeRemaining = 0;
+
     private void Start()
     {
         renderer = gameObject.GetComponent<Renderer>();
-        renderer.material.color = Color.red;
+    }
+
+    private void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            ChangeColour();
+            timeRemaining += 1f;
+        }
+    }
+
+    private void ChangeColour()
+    {
+        Color randomColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        renderer.material.color = randomColor;
     }
 }
